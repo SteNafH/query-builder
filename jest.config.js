2f1c4738d31ac263f75a1d753a3ed5b08b789467
SteNafH <stefanh2005@gmail.com>
2023-07-20 13:53:35 +0200

    Initial Commit

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    silent: false,
+    verbose: false,
+    collectCoverage: true,
+    collectCoverageFrom: ["src/**/*.ts"],
+};
